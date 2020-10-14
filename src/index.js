4db64ea82796d1fb848134687a728b7ff3be11df
DenisProkopchik <prokopchik.1996@mail.ru>
2020-10-14 22:24:07 +0300

    feat: done tatsks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = [];
+  let openPar = bracketsConfig.reduce((sum, item, index) => sum = `${sum} ${item[0]}${index}`, '').split(' ').slice(1);
+  let closedPar = bracketsConfig.reduce((sum, item, index) => sum = `${sum} ${item[1]}${index}`, '').split(' ').slice(1);
+  str.split('').forEach(item => {
+    let o = openPar.find(item2 => item === item2.slice(0, 1));
+    let c = closedPar.find(item2 => item === item2.slice(0, 1));
+
+    if (c && result.length > 0) {
+      result[result.length - 1].slice(-1) === c.slice(-1) ? result.splice(-1) : c === o ? result.push(o) : result;
+    } else {
+      result.push(o);
+    }
+    return result
+  });
+
+  return result.length > 0 ? false : true;
 }
